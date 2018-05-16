7d1630be9a920f9e8493c014f86a27aeb8e86c66
Michał M <maaslo77@gmail.com>
2018-05-16 15:14:09 +0200

    Add files via upload

---
+++ Systemy Operacyjne/11/4.c
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+ 
+#define MAX 100
+#define KEY 1234
+ 
+ int main(){
+     int shmid;
+     void *shared_memory = (void *)0;
+     shmid = shmget(KEY, MAX, 0666 |IPC_CREAT);
+     shared_memory = shmat(shmid, NULL,0);
+     sprintf(shared_memory, "32768");
+    
+ exit(0);
+ }
